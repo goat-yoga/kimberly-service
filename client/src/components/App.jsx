ac300dc6cf3082b77e9ec9e2818fc0c8158db57d
Kimberly Eddinger <kimberly.c.eddinger@gmail.com>
2020-06-24 20:13:46 -0700

    Sets up webpack and bundle, adds new React components to client.

---
+++ client/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // add state
+    }
+  }
+  render(){
+    return (
+      <div>
+        Hello from React
+      </div>
+    )
+  }
+}
+
+export default App;
